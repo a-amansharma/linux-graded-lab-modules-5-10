9e6290ad8b8ae1e631bb61b24225a7ec6dee9c96
Aman Sharma <amanxrishi@gmail.com>
2026-01-30 15:52:32 +0530

    question 9 done

---
+++ Question9/zombie.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    for(int i=0;i<3;i++){
+        if(fork()==0){
+            _exit(0);
+        }
+    }
+
+    while(wait(NULL)>0){
+        printf("Cleaned child process\n");
+    }
+    return 0;
+}
